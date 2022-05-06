3b0c8de228a5b530c45ea408a11a4e391b0c5eaf
Maxwell Morrow <maxdmorrow@yahoo.com>
2022-05-05 20:57:18 -0700

    user sequelize CLI to create model and connection, did my first migration some code removed from the manual way of doing it

--- server.js
+++ server.js
@@ -8,16 +8,18 @@ require('dotenv').config()
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
+// ALL OF THE COMMENTED OUT CODE BELOW is no longer needed when using the sequelize CLI to instantiate the connection
+
 // SEQUELIZE CONNECTION
 // place this above anywhere the database is needed. 
-const sequelize = new Sequelize(process.env.PG_URI)
+// const sequelize = new Sequelize(process.env.PG_URI)
 
-try {
-    sequelize.authenticate() 
-    console.log(`Connected with Sequelize at ${process.env.PG_URI}`) 
-} catch(err) {
-    console.log(`Unable to connect to PG: ${err}`) 
-}
+// try {
+//     sequelize.authenticate() 
+//     console.log(`Connected with Sequelize at ${process.env.PG_URI}`) 
+// } catch(err) {
+//     console.log(`Unable to connect to PG: ${err}`) 
+// }
 
 // ROOT
 app.get('/', (req, res) => {
