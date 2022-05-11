5dcf291600b72eb30e517db400b7faa552057dd2
Maxwell Morrow <maxdmorrow@yahoo.com>
2022-05-10 20:30:56 -0700

    finished association queries

--- controllers/bands_controller.js
+++ controllers/bands_controller.js
@@ -1,4 +1,3 @@
-
 // sequelize helper methods
 // 1. findAll()
 // 2. findOne()
@@ -6,108 +5,127 @@
 // 4. update()
 // 5. destroy()
 
-
-
 // DEPENDENCIES
 // this is an express server we are using to route
 const bands = require("express").Router();
 // grabing the models folder here gives us access to all of the models
 const db = require("../models");
 // destructuring bands here to keep us from having to specify which db each time
-const { Band } = db;
-const {Op} = require("sequelize")
-
+const { Band, MeetGreet, Event, SetTime } = db;
+const { Op } = require("sequelize");
 
 // 1. FIND ALL BANDS
 // this starts with bands.get because thats the server we are making a request on im not sure about this part.
-bands.get('/', async (req, res) => {
-    try {
-        // saving the found things to a variable.
-        // notice we use Band.findAll() here because we destructed the specific table we wanted above.
-        const foundBands = await Band.findAll(
-            // this syntax is confusing af definitely dont try and do this without documentation. 
-            {
-                // this is ordering our data fairly straightforward
-            order: [["available_start_time", "ASC"]],
-            where: {
-                // this is using query params to get specific bands using a ternary
-                // the ternary uses query params if there are some and using an empty string iff query params are null.
-                name: {[Op.like]: `%${req.query.name ? req.query.name : ""}%`}
-            }
-        })
+bands.get("/", async (req, res) => {
+  try {
+    // saving the found things to a variable.
+    // notice we use Band.findAll() here because we destructed the specific table we wanted above.
+    const foundBands = await Band.findAll(
+      // this syntax is confusing af definitely dont try and do this without documentation.
+      {
+        // this is ordering our data fairly straightforward
+        order: [["available_start_time", "ASC"]],
+        where: {
+          // this is using query params to get specific bands using a ternary
+          // the ternary uses query params if there are some and using an empty string iff query params are null.
+          name: { [Op.like]: `%${req.query.name ? req.query.name : ""}%` },
+        },
+      }
+    );
 
-
-        // including these res.status codes is best practice.
-        res.status(200).json(foundBands)
-    } 
+    // including these res.status codes is best practice.
+    res.status(200).json(foundBands);
+  } catch (error) {
     // this is a catch statement pretty standard for all of our request.
-    catch (error) {
-        res.status(500).json(error)
-    }
-})
+    res.status(500).json(error);
+  }
+});
 
 // 2. FIND A SPECIFIC BAND
-bands.get('/:id', async (req, res) => {
-    try {
-        const foundBand = await Band.findOne({
-           
-            where: { band_id: req.params.id }
-        })
-        res.status(200).json(foundBand)
-        
-    } catch (error) {
-        res.status(500).json(error)
-    }
-})
+bands.get("/:name", async (req, res) => {
+  try {
+    const foundBand = await Band.findOne({
+      where: { band_id: req.params.name },
+      include: [
+        {
+          model: MeetGreet, //   this includes any meetGreet data associated with named band
+          as: "meet_greets",
+          include: {
+            model: Event, //  This is a nested includes events associated with the meetGreet data
+            as: "event",
+            where: {
+              name: {
+                [Op.like]: `%${req.query.event ? req.query.event : ""}%`, //  This filters the event by name in the query params
+              },
+            },
+          },
+        },
+        {
+          model: SetTime, //  This includes set times
+          as: "set_times",
+          include: {
+            model: Event,
+            as: "event",
+            where: {
+              name: {
+                [Op.like]: `%${req.query.event ? req.query.event : ""}%`, //  This filters the event by name in the query params
+              },
+            },
+          },
+        },
+      ],
+    });
+    res.status(200).json(foundBand);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
 
 // 3. CREATE A BAND
-bands.post('/', async (req, res) => {
-    try {
-        const newBand = await Band.create(req.body)
-        res.status(200).json({
-            // this is just a json response this might need to get more complicated for different apps.
-            message: 'Successfully inserted a new band',
-            data: newBand
-        })
-    } catch(err) {
-        res.status(500).json(err)
-    }
-})
+bands.post("/", async (req, res) => {
+  try {
+    const newBand = await Band.create(req.body);
+    res.status(200).json({
+      // this is just a json response this might need to get more complicated for different apps.
+      message: "Successfully inserted a new band",
+      data: newBand,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 // 4. UPDATE A BAND
-bands.put('/:id', async (req, res) => {
-    try {
-        const updatedBands = await Band.update(req.body, {
-            where: {
-                band_id: req.params.id
-            }
-        })
-        res.status(200).json({
-            message: `Successfully updated ${updatedBands} band(s)`
-        })
-    } catch(err) {
-        res.status(500).json(err)
-    }
-})
+bands.put("/:id", async (req, res) => {
+  try {
+    const updatedBands = await Band.update(req.body, {
+      where: {
+        band_id: req.params.id,
+      },
+    });
+    res.status(200).json({
+      message: `Successfully updated ${updatedBands} band(s)`,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 // 5. DELETE A BAND
-bands.delete('/:id', async (req, res) => {
-    try {
-        const deletedBands = await Band.destroy({
-            where: {
-                band_id: req.params.id
-            }
-        })
-        res.status(200).json({
-            message: `Successfully deleted ${deletedBands} band(s)`
-        })
-    } catch(err) {
-        res.status(500).json(err)
-    }
-})
-
-
-
+bands.delete("/:id", async (req, res) => {
+  try {
+    const deletedBands = await Band.destroy({
+      where: {
+        band_id: req.params.id,
+      },
+    });
+    res.status(200).json({
+      message: `Successfully deleted ${deletedBands} band(s)`,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 // EXPORT
 module.exports = bands;
